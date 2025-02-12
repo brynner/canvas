984edec2641d2244965eaae04dcb387e0a4d3849
brynner <br7nner@gmail.com>
2025-02-12 00:24:30 -0300

    App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import AnnotationCanvas from './components/AnnotationCanvas';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const fabricCanvasRef = useRef(null);
+
+    return (
+        <div className="app">
+            <AnnotationCanvas ref={fabricCanvasRef} />
+        </div>
+    );
+};
 
 export default App;
